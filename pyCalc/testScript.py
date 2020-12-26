d134dd1ef0a91bc19c994dd943bcefad70bf12ab
Amedeo Fortino <amedeo.fortino@gmail.com>
2020-12-26 21:49:52 +0100

    First Commit of the package

---
+++ pyCalc/testScript.py
@@ -0,0 +1,10 @@
+def hello_world(args: object) -> None:
+    """
+    Prints the argument passed in a sentence.
+    :param args:
+        String or whatever to print
+    :return:
+        None
+    """
+
+    print(f"Thank you friend for giving me a {args}")
